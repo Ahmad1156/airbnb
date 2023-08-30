baf0c3606bafe7e1b4d5fa15875aa9388dcca32e
ahmad shakik <ahmadshakik777@gmail.com>
2023-08-30 19:29:19 +0300

    half the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,36 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import RegisterModal from "./components/modals/RegisterModal";
+import LoginModal from "./components/modals/LoginModal";
+import ToasterProvider from "./providers/ToastProvider";
+import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "./components/modals/RentModal";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Airbnb",
   description: "Airbnb clone",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ToasterProvider />
+        <RegisterModal />
+        <RentModal />
+        <LoginModal />
+        <Navbar currentUser={currentUser} />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="px-5 mt-5 border border-red-400">hello</div>;
+  return <div className=""></div>;
 }
